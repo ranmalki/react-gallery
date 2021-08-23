ee111c8c86f4fdaf15ff66da5e75a32cbc6eb2b2
Ran Malki <ran.malki2@gmail.com>
2021-08-23 03:12:11 +0300

    HW 16/8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [albums, setAlbums] = useState([]);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(res => res.json())
+      .then(data => setAlbums(data))
+  }, [])
+
+  const selectAlbum = (event) => {
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${event.target.selectedIndex}`)
+      .then(res => res.json())
+      .then(data => setItems(data))
+  }
+
   return (
     <div className="App">
       <h1>Select an album:</h1>
+      <select name="albums" id="albums" onChange={selectAlbum}>
+        {albums.map((album, index) => <option value={album.id} key={index}>{album.title}</option>)}
+      </select>
+      <br />
+      <br />
+      {items.map((item, index) => <img key={index} src={item.thumbnailUrl} alt={item.title} />)}
     </div>
   );
 }
